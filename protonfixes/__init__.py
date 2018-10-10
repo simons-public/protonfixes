575d415ce3d9f67762590c332cc5d1abf8948e1f
Chris Simons <35010457+simons-public@users.noreply.github.com>
2018-10-10 13:22:57 -0700

    Remove 'STEAMSCRIPT' env var requirement
    Not every environment has the STEAMSCRIPT environment variable

--- protonfixes/__init__.py
+++ protonfixes/__init__.py
@@ -8,6 +8,5 @@ from . import fix
 if 'DEBUG' in os.environ:
     from . import debug
 
-if 'STEAMSCRIPT' in os.environ:
-    if 'iscriptevaluator.exe' not in sys.argv[2]:
-        fix.main()
+if 'iscriptevaluator.exe' not in sys.argv[2]:
+    fix.main()
