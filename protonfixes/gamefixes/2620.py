edef4981b94b54f10ad28a6f8dc6286208d18086
Patryk Obara <dreamer.tan@gmail.com>
2018-10-15 22:53:23 +0200

    Fix Call of Duty gamefix
    Problem found using pylint:
    Module 'protonfixes.util' has no 'set_environ' member

--- protonfixes/gamefixes/2620.py
+++ protonfixes/gamefixes/2620.py
@@ -12,5 +12,5 @@ def main():
     log('Applying fixes for Call of Duty (2003')
 
     # https://github.com/ValveSoftware/Proton/pull/1423/commits/1a1d25c7d95691e37c94aea4e5f94e0c917aba6f
-    util.set_environ('MESA_EXTENSION_MAX_YEAR', '2003')
-    util.set_environ('__GL_ExtensionStringVersion', '17700')
+    util.set_environment('MESA_EXTENSION_MAX_YEAR', '2003')
+    util.set_environment('__GL_ExtensionStringVersion', '17700')
