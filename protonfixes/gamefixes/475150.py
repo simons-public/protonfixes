cc11c082aeb441adf90f56ea904f5037872ac453
MrCraigen <johannes.svedqvist@gmail.com>
2018-10-15 23:14:54 +0200

    Added fix for Titan Quest Anniversary Edition

---
+++ protonfixes/gamefixes/475150.py
@@ -0,0 +1,14 @@
+""" Game fix for Titan Quest Anniversary Edition(475150)
+"""
+#pylint: disable=C01033
+
+from protonfixes import util
+from protonfixes.logger import log
+
+def main():
+    """ Set OS to Windows XP to pass the black menu screen
+    """
+
+    log('Applying Titan Quest Anniversary Edition Game Fixes')
+
+    util.protontricks('winxp')
