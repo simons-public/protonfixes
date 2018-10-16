a00adec88fbc1d1d3af63f0e9a72687fa653bcd3
Chris Simons <chris+git@simonsmail.net>
2018-10-16 06:35:06 -0700

    Version bump for pypi

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ print(data_files)
 
 setup(
     name='protonfixes',
-    version='1.0.11',
+    version='1.0.12',
     description='Python module for applying fixes at runtime for games not supported by Steam Proton',
     long_description=long_description,
     long_description_content_type='text/markdown',
