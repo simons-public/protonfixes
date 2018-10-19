00a434555e3f63d7bf4c5fef4dd540c80b1d821a
pchome <pchome@users.noreply.github.com>
2018-10-19 21:20:40 +0300

    Added automated game name logging
    Example:
    ```
    ProtonFixes[4276] INFO: Running protonfixes
    ProtonFixes[4276] INFO: Using local protonfix for "The Witcher 3: Wild Hunt" (292030)
    ```
    
    Fixes #22

--- protonfixes/fix.py
+++ protonfixes/fix.py
@@ -24,28 +24,46 @@ def game_id():
     return None
 
 
+def game_name():
+    """ Trys to return the game name from environment variables
+    """
+
+    game_library = re.findall(r'.*/steamapps', os.environ['PWD'], re.IGNORECASE)[-1]
+    game_manifest = os.path.join(game_library, 'appmanifest_' + game_id() + '.acf')
+    try:
+        with open(game_manifest, 'r') as appmanifest:
+            for x in appmanifest.readlines():
+                if 'name' in x.strip():
+                    name = re.findall(r'"[^"]+"', x)[-1]
+                    return name
+    except OSError:
+        return 'UNKNOWN'
+    return 'UNKNOWN'
+
+
 def run_fix(gameid):
     """ Loads a gamefix module by it's gameid
     """
 
     if gameid is not None:
+        game = game_name() + ' ('+ gameid + ')'
         localpath = os.path.expanduser('~/.config/protonfixes/localfixes')
         if os.path.isfile(os.path.join(localpath, gameid + '.py')):
             open(os.path.join(localpath, '__init__.py'), 'a').close()
             sys.path.append(os.path.expanduser('~/.config/protonfixes'))
             try:
                 game_module = import_module('localfixes.' + gameid)
-                log.info('Using local protonfix for gameid ' + gameid)
+                log.info('Using local protonfix for ' + game)
                 game_module.main()
             except ImportError:
-                log.info('No local protonfix found for gameid ' + gameid)
+                log.info('No local protonfix found for ' + game)
         else:
             try:
                 game_module = import_module('protonfixes.gamefixes.' + gameid)
-                log.info('Using protonfix for gameid ' + gameid)
+                log.info('Using protonfix for ' + game)
                 game_module.main()
             except ImportError:
-                log.info('No protonfix found for gameid ' + gameid)
+                log.info('No protonfix found for ' + game)
 
 
 def main():
