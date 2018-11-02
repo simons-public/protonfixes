9d2b594fb1971dd7ca4737a7fe5cde33714ec7ab
Chris Simons <chris+git@simonsmail.net>
2018-11-01 20:24:20 -0700

    Added proper traceback for troubleshooting when errors occur

--- protonfixes/__init__.py
+++ protonfixes/__init__.py
@@ -3,10 +3,18 @@
 
 import os
 import sys
+import traceback
 from . import fix
 
 if 'DEBUG' in os.environ:
     from . import debug
 
 if 'STEAM_COMPAT_DATA_PATH' in os.environ:
-    fix.main()
+    try:
+        fix.main()
+
+    #pylint: disable=W0702
+    # Catch any exceptions and print a traceback
+    except:
+        sys.stderr.write('ProtonFixes ' + traceback.format_exc())
+        sys.stderr.flush()
