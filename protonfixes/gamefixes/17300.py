9f62ac1e01b878edf0ac987499b5d856a63e9ba8
Chris Simons <35010457+simons-public@users.noreply.github.com>
2018-11-04 14:05:37 -0800

    Remove trailing whitespace

--- protonfixes/gamefixes/17300.py
+++ protonfixes/gamefixes/17300.py
@@ -10,6 +10,6 @@ def main():
 
     # https://github.com/ValveSoftware/Proton/issues/178#issuecomment-422986182
     util.protontricks('d3dcompiler_43')
-    
+
     # https://github.com/ValveSoftware/Proton/issues/178#issuecomment-415201326
     util.disable_esync()
