c5025290cdada85f2a8cedd1a5e13ff2356cd62b
NoXPhasma <noxphasma@live.de>
2018-12-09 19:04:34 +0100

    Added fix for Portal Knights

---
+++ protonfixes/gamefixes/374040.py
@@ -0,0 +1,14 @@
+from protonfixes import util
+
+
+def main():
+    """ Install xact and override xaudio2_7 to native
+    """
+
+    print('Applying fixes for Portal Knights')
+
+    # install xact
+    util.protontricks('xact')
+
+    # set xaudio2_7.dll to native
+    util.winedll_override('xaudio2_7', 'n')
