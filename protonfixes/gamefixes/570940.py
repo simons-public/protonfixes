63b4bc320c24e68feb0baaf54c36c5035891bdd0
Chris Simons <chris@simonsmail.net>
2019-06-19 20:30:17 -0700

    linting

--- protonfixes/gamefixes/570940.py
+++ protonfixes/gamefixes/570940.py
@@ -1,9 +1,9 @@
-""" Game fix Dark Souls Remastered 
+""" Game fix Dark Souls Remastered
 """
 #pylint: disable=C0103
 
 from protonfixes import util
 
 def main():
-    # Requires vcrun2017 to launch
+    """ Requires vcrun2017 to launch """
     util.protontricks('vcrun2017')
