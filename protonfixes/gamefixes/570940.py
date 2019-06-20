1dacb2c0fb50640355a12a13def064fbbedd4d48
Tobias Langendorf <tobias.langendorf@googlemail.com>
2019-06-20 05:29:31 +0200

    Fix for Dark Souls Remastered (#78)

---
+++ protonfixes/gamefixes/570940.py
@@ -0,0 +1,9 @@
+""" Game fix Dark Souls Remastered 
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    # Requires vcrun2017 to launch
+    util.protontricks('vcrun2017')
