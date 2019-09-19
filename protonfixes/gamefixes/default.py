7cd24ad53ca09b24d3c3330d5e8eee13753003cc
pchome <pchome@users.noreply.github.com>
2019-09-20 01:46:50 +0300

    Hi, Travis!

--- protonfixes/gamefixes/default.py
+++ protonfixes/gamefixes/default.py
@@ -1,13 +1,14 @@
-from protonfixes import util
-import os
 import sys
+from protonfixes import util
 
 def main():
     """ global defaults
     """
-    
+
     # Steam commandline
     def use_steam_commands():
+        """ Parse aliases from Steam launch options
+        """
         pf_alias_list = list(filter(lambda item: '-pf_' in item, sys.argv))
 
         for pf_alias in pf_alias_list:
@@ -17,7 +18,7 @@ def main():
             elif pf_alias == '-pf_regedit':
                 util.regedit()
             elif pf_alias.split('=')[0] == '-pf_tricks':
-                param = str(pf_alias.replace('-pf_tricks=',''))
+                param = str(pf_alias.replace('-pf_tricks=', ''))
                 util.protontricks(param)
-        
+
     use_steam_commands()
