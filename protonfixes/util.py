c74c63ada37bce2437c76cefc437d7a3505e3975
BlueBoxWare <BlueBoxWare@users.noreply.github.com>
2020-01-29 05:04:55 +0100

    util.disable_uplay_overlay() (#92)
    * util.disable_uplay_overlay()
    
    Utility function to disable the UPlay in-game overlay.
    
    * util.once
    
    Add a decorator which can be used on functions which should only run
    once per prefix.
    
    * Fix linting

--- protonfixes/util.py
+++ protonfixes/util.py
@@ -10,6 +10,7 @@ import signal
 import zipfile
 import subprocess
 import urllib.request
+import functools
 from .logger import log
 from . import config
 
@@ -84,6 +85,50 @@ def protonversion(timestamp=False):
         return protontimeversion()
     return protonnameversion()
 
+def once(func=None, retry=None):
+    """ Decorator to use on functions which should only run once in a prefix.
+
+    Error handling:
+    By default, when an exception occurs in the decorated function, the
+    function is not run again. To change that behavior, set retry to True.
+    In that case, when an exception occurs during the decorated function,
+    the function will be run again the next time the game is started, until
+    the function is run successfully.
+
+    Implementation:
+    Uses a file (one per function) in PROTONPREFIX/drive_c/protonfixes/run/
+    to track if a function has already been run in this prefix.
+    """
+    if func is None:
+        return functools.partial(once, retry=retry)
+    retry = retry if retry else False
+
+    #pylint: disable=missing-docstring
+    def wrapper(*args, **kwargs):
+        func_id = func.__module__ + "." + func.__name__
+        prefix = protonprefix()
+        directory = os.path.join(prefix, "drive_c/protonfixes/run/")
+        file = os.path.join(directory, func_id)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        if os.path.exists(file):
+            return
+
+        exception = None
+        try:
+            func(*args, **kwargs)
+        except Exception as exc: #pylint: disable=broad-except
+            if retry:
+                raise exc
+            exception = exc
+
+        open(file, 'a').close()
+
+        if exception:
+            raise exception #pylint: disable=raising-bad-type
+
+        return
+    return wrapper
 
 def _killhanging():
     """ Kills processes that hang when installing winetricks
@@ -441,6 +486,36 @@ def disable_fsync(): # pylint: disable=missing-docstring
 def disable_d3d11():  # pylint: disable=missing-docstring
     set_environment('PROTON_NO_D3D11', '1')
 
+@once
+def disable_uplay_overlay():
+    """Disables the UPlay in-game overlay.
+
+    Creates or appends the UPlay settings.yml file
+    with the correct setting to disable the overlay.
+    UPlay will overwrite settings.yml on launch, but keep
+    this setting.
+    """
+    config_dir = os.path.join(
+        protonprefix(),
+        'drive_c/users/steamuser/Local Settings/Application Data/Ubisoft Game Launcher/'
+    )
+    config_file = os.path.join(config_dir, 'settings.yml')
+
+    if not os.path.isdir(config_dir):
+        log.warn(
+            'Could not disable UPlay overlay: "'
+            + config_dir
+            + '" does not exist or is not a directory.'
+        )
+        return
+
+    try:
+        with open(config_file, 'a+') as file:
+            file.write("\noverlay:\n  enabled: false\n")
+        log.info('Disabled UPlay overlay')
+        return
+    except OSError as err:
+        log.warn('Could not disable UPlay overlay: ' + err.strerror)
 
 def create_dosbox_conf(conf_file, conf_dict):
     """Create DOSBox configuration file.
