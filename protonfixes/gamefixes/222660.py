e212f91d5cf0b4c342bff8d8fee1bfaf0a7c883e
mrjackv <1vercesig@gmail.com>
2020-01-30 14:29:47 +0100

    Add fix for Retro/Grade

---
+++ protonfixes/gamefixes/222660.py
@@ -0,0 +1,12 @@
+""" Game fix for Retro/Grade
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+
+def main():
+    """ Install windowscodecs
+    """
+
+    util.protontricks('windowscodecs')
