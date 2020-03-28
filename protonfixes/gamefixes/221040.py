71da369d5d6357cb20a4f2841d419e9919012e69
soredake <soredake@users.noreply.github.com>
2020-03-28 06:25:57 +0200

    deprecate and add batch of fixes (#120)

---
+++ protonfixes/gamefixes/221040.py
@@ -0,0 +1,11 @@
+""" Game fix for Resident Evil 6 / Biohazard 6
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Uses wmp11
+    """
+
+    util.protontricks('wmp11')
---
+++ protonfixes/gamefixes/339340.py
@@ -0,0 +1,11 @@
+""" Game fix for Resident Evil 0 / biohazard 0 HD REMASTER
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Uses wmp11
+    """
+
+    util.protontricks('wmp11')
