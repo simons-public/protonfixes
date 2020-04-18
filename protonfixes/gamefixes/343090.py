1b6557e45cd3ac489be5a050ab70d1b1c7ce48ae
Joshua Fern <JoshuaFern@protonmail.com>
2020-04-18 22:35:19 +0000

    Add fixes for SimpleRockets (#123)

---
+++ protonfixes/gamefixes/343090.py
@@ -0,0 +1,13 @@
+""" Game fix for SimpleRockets
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Uses winetricks to install dotnet40
+        https://appdb.winehq.org/objectManager.php?sClass=version&iId=34014
+    """
+
+    # If not already installed, install dotnet40
+    util.protontricks('dotnet40')
