ec95fd5a92816f0eed002b11bd21efb96d0e564f
Michael Pobega <pobega@gmail.com>
2020-06-13 18:41:51 -0400

    add fixes for Persona 4 Golden

---
+++ protonfixes/gamefixes/1113000.py
@@ -0,0 +1,13 @@
+""" Game fix for Persona 4 Golden
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ installs devenum, quartz, wmp9
+    """
+
+    util.protontricks('devenum')
+    util.protontricks('quartz')
+    util.protontricks('wmp9')
