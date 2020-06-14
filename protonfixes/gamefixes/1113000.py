f5bf522b35308be266e2b56ada112ed169ff8c38
Michael 'pbone' Pobega <Pobega@users.noreply.github.com>
2020-06-14 16:45:14 -0400

    add fixes for Persona 4 Golden (#128)
    * add fixes for Persona 4 Golden
    
    * Persona 4 Golden needs Pulse latency change for crackling audio

---
+++ protonfixes/gamefixes/1113000.py
@@ -0,0 +1,17 @@
+""" Game fix for Persona 4 Golden
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ installs devenum, quartz, wmp9 and adjust pulse latency
+    """
+
+    # Fix pre-rendered cutscene playback
+    util.protontricks('devenum')
+    util.protontricks('quartz')
+    util.protontricks('wmp9')
+
+    # Fix crackling audio
+    util.set_environment('PULSE_LATENCY_MSEC', '60')
