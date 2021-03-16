b54cb92ee02861ba5bde4752439326f13263fb7f
soredake <5204968+soredake@users.noreply.github.com>
2021-03-16 18:43:36 +0200

    Add game fixes for Space Rangers 1/2 to allow widescreen patcher to work

---
+++ protonfixes/gamefixes/46320.py
@@ -0,0 +1,12 @@
+""" Space Rangers
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+
+def main():
+    """ Install vb6run
+    """
+
+    util.protontricks('vb6run')
---
+++ protonfixes/gamefixes/46330.py
@@ -0,0 +1,12 @@
+""" Space Rangers 2: Reboot
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+
+def main():
+    """ Install vb6run
+    """
+
+    util.protontricks('vb6run')
