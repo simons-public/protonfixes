46149db7533e00fdbe6eb22ef53fdeba72a33691
Lucas Mindêllo de Andrade <lucas@mindello.com.br>
2022-04-19 09:10:12 -0300

    chore(lint): line size

--- protonfixes/gamefixes/1097150.py
+++ protonfixes/gamefixes/1097150.py
@@ -8,8 +8,19 @@ def main():
     """ Create symlink of eac so at the right location
     """
     if os.path.exists('FallGuys_client_game_Data/Plugins/x86_64/easyanticheat_x64.so'):
-        subprocess.call(['rm', '-rf', 'FallGuys_client_game_Data/Plugins/x86_64/easyanticheat_x64.so'])
-    subprocess.call(['ln', '-s', '../../../EasyAntiCheat/easyanticheat_x64.so', 'FallGuys_client_game_Data/Plugins/x86_64/easyanticheat_x64.so'])
+        subprocess.call([
+            'rm', 
+            '-rf', 
+            'FallGuys_client_game_Data/Plugins/x86_64/easyanticheat_x64.so'])
+    subprocess.call([
+        'ln', 
+        '-s', 
+        '../../../EasyAntiCheat/easyanticheat_x64.so', 
+        'FallGuys_client_game_Data/Plugins/x86_64/easyanticheat_x64.so'])
 
     # Fixes the ini file.
-    subprocess.call(['sed', '-i', 's/TargetApplicationPath=FallGuysEACLauncher.exe/TargetApplicationPath=FallGuys_client_game.exe/', 'FallGuys_client.ini'])
+    subprocess.call([
+        'sed',
+        '-i', 
+        's/TargetApplicationPath=FallGuysEACLauncher.exe/TargetApplicationPath=FallGuys_client_game.exe/', 
+        'FallGuys_client.ini'])
