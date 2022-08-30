2467125594bc879bec524f87fed31f8822d5db89
Sifi <sifiro@outlook.com>
2022-08-30 09:55:11 +0200

    Add protonfix for Littlewitch Romanesque: Editio Regia (349300)

---
+++ protonfixes/gamefixes/349300.py
@@ -0,0 +1,11 @@
+""" Fix for Littlewitch Romanesque: Editio Regia (349300)
+"""
+
+from protonfixes import util
+
+def main():
+	""" Run the game from S: avoids some file reading issues.
+	(The game deletes dots from filepath reading, which usually It's a issue in a SteamLinux standard directory)
+	https://github.com/ValveSoftware/Proton/issues/5975#issuecomment-1178969055
+	"""
+	set_environment('PROTON_SET_GAME_DRIVE','1')
