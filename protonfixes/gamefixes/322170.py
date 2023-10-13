368b40ceb9ce6cbf4c2b2b50f61001bc39325c80
The Bearodactyl <114454115+TheBearodactyl@users.noreply.github.com>
2023-10-13 20:51:41 +0000

    Add Geometry Dash

---
+++ protonfixes/gamefixes/322170.py
@@ -0,0 +1,22 @@
+""" Game fix for Geometry Dash
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+from protonfixes.logger import log
+
+def main():
+    """ Changes the proton argument from the launcher to the game
+    """
+
+    log('Applying fixes for Geometry Dash')
+
+    # Fix FSYNC and ESYNC being enabled
+    util.set_environment('PROTON_NO_ESYNC', '1')
+    util.set_environment('PROTON_NO_FSYNC', '1')
+
+    # Me when single-core games:
+    util.append_argument('-USEALLAVAILABLECORES')
+
+    # Geode
+    util.winedll_override('xinput9_1_0', 'n', 'b')
